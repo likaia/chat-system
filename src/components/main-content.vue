15f5bdcbe7e4c3752a7b44bd8b12ddeff2578da1
likai <1195419506@qq.com>
2019-10-22 00:01:57 +0800

    完善顶部菜单栏布局

--- src/components/main-content.vue
+++ src/components/main-content.vue
@@ -1,16 +1,17 @@
 <template>
     <div id="main-content">
         <div class="top-panel">
+            <!--左侧图标-->
             <div class="left-panel">
-                <div class="icon-panel">
+                <div class="icon-panel" @mouseover="showLeftIco(true)" @mouseleave="showLeftIco(false)">
                     <div class="fillet-ico-panel close-ico">
-
+                        <img :style="{display:leftIco}" src="../assets/img/close.png"/>
                     </div>
                     <div class="fillet-ico-panel mini-ico">
-
+                        <img :style="{display:leftIco}" src="../assets/img/min.png" />
                     </div>
                     <div class="fillet-ico-panel max-ico">
-
+                        <img  :style="{display:leftIco}" src="../assets/img/max.png" />
                     </div>
                 </div>
                 <div class="search-panel">
@@ -20,6 +21,28 @@
                     </div>
                     <div class="add-panel" @mouseleave="createGroupChat(false)" @mouseup="createGroupChat(false)" @mousedown="createGroupChat(true)" :style="{background: addBackground}">
                         <img src="../assets/img/create_discuss_simple@2x.png"/>
+                    </div>
+                </div>
+            </div>
+            <div class="right-panel">
+                <!--功能切换-->
+                <div class="function-switching-panel">
+                    <div class="main-ico-area" @click="menuSwitch(0)">
+                       <img src="../assets/img/menu-message-down@2x.png" />
+                    </div>
+                    <div class="main-ico-area" @click="menuSwitch(1)">
+                        <img src="../assets/img/menu-contact-normal@2x.png" />
+                    </div>
+                    <div class="main-ico-area" @click="menuSwitch(2)">
+                        <img src="../assets/img/menu-more-normal@2x.png" />
+                    </div>
+                </div>
+                <!--头像区域-->
+                <div class="avatar-area-panel">
+                    <!--在线状态-->
+                    <div class="status-panel" @click="onlineStatus"></div>
+                    <div class="avatar-panel">
+                        <img src="../assets/img/avatar.jpg"/>
                     </div>
                 </div>
             </div>
@@ -34,7 +57,11 @@
             return{
                 searchImg:"block",
                 searchContent:"",
-                addBackground:"#ffffff"
+                addBackground:"#ffffff",
+                leftIco:"none",
+                messageIco:"../assets/img/menu-message-down@2x.png",
+                contactIco:"../assets/img/menu-contact-normal@2x.png",
+                moreIco:"../assets/img/menu-more-normal@2x.png"
             }
         },
         methods:{
@@ -58,7 +85,35 @@
                 }else{
                     this.addBackground = "#ffffff";
                 }
+            },
+            // 显示左上角图标
+            showLeftIco:function(status){
+              if(status){
+                  this.leftIco = "block";
+              }else{
+                  this.leftIco = "none";
+              }
+            },
+            // 在线状态
+            onlineStatus:function(){
 
+            },
+            // 功能切换
+            menuSwitch:function(item){
+                console.log(item);
+                if(item===0){
+                    this.messageIco = "../assets/img/menu-message-down@2x.png";
+                    this.contactIco = "../assets/img/menu-contact-normal@2x.png";
+                    this.moreIco = "../assets/img/menu-more-normal@2x.png";
+                }else if(item===1){
+                    this.messageIco = "../assets/img/menu-contact-normal@2x.png";
+                    this.contactIco = "../assets/img/menu-contact-down@2x.png";
+                    this.moreIco = "../assets/img/menu-more-normal@2x.png";
+                }else if(item===2){
+                    this.messageIco = "../assets/img/menu-contact-normal@2x.png";
+                    this.contactIco = "../assets/img/menu-contact-normal@2x.png";
+                    this.moreIco = "../assets/img/menu-more-down@2x.png";
+                }
             },
             // 非空判断
             isEmpty:(str)=>{
