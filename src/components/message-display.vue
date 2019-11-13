ccc9c7b093e03bdf07152c907f14217354384aad
likai <1195419506@qq.com>
2019-11-13 18:14:00 +0800

    win10提交:完善消息列表页面更新

--- src/components/message-display.vue
+++ src/components/message-display.vue
@@ -1,7 +1,27 @@
 <template>
-    <div>
-        当前用户id: {{userId}}
+    <div id="mainContent">
+        <div class="top-panel">
+            <div class="title-panel">
+                <p>用户id{{userId}}</p>
+                <!--在线设备类型-->
+                <div class="equipmentType">
+
+                </div>
+            </div>
+            <div class="operate-panel">
+                <div class="ico-panel">
+                    <div class="item-panel" @mouseenter="createDisEventFun('hover')" @mouseleave="createDisEventFun('leave')" @click="createDisEventFun('click')">
+                        <img :src="createDisSrc" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--消息显示-->
+        <div class="messages-panel">
+
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -9,7 +29,25 @@
         name: "message-display",
         data(){
             return{
-                userId:this.$route.params.userId
+                userId:this.$route.params.userId,
+                createDisSrc:require("../assets/img/titlebar_function_createDis_normal@2x.png"),
+                resourceObj:{
+                    createDisNormal:require("../assets/img/titlebar_function_createDis_normal@2x.png"),
+                    createDisHover:require("../assets/img/titlebar_function_createDis_hover@2x.png"),
+                    createDisClick:require("../assets/img/titlebar_function_createDis_normal_p@2x.png")
+                }
+            }
+        },
+        methods:{
+            createDisEventFun:function(status){
+                if(status==="hover"){
+                    this.createDisSrc = this.resourceObj.createDisHover
+                }else if(status==="leave"){
+                    this.createDisSrc = this.resourceObj.createDisNormal
+                }else{
+                    this.createDisSrc = this.resourceObj.createDisClick
+                }
+
             }
         },
         beforeRouteUpdate(to,form,next){
@@ -20,6 +58,4 @@
     }
 </script>
 
-<style scoped>
-
-</style>
+<style lang="scss" src="../assets/scss/message-display.scss" scoped></style>
