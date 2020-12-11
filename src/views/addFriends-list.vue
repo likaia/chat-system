5893e7a5b4fac0f73ce96bd075f2eaa383563b83
wenhim <6531221+wenhim@user.noreply.gitee.com>
2020-12-11 15:41:12 +0800

    fix(好友列表): 添加好友
    1.修复弹出框不可拖曳的问题

--- src/views/addFriends-list.vue
+++ src/views/addFriends-list.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="addFriends-list-outer-mask">
-    <div class="addFriends-list-content" ref="addFriendsListContent">
+    <div
+      class="addFriends-list-content"
+      ref="addFriendsListContent"
+      @mousedown="alertDown($event)"
+      @mousemove="alertMove($event)"
+      @mouseup="alertUp"
+      @blur="alertUp"
+    >
       <div class="top-panel">
         <div class="top-panel-left">
           <div
@@ -49,6 +56,8 @@
               v-model="searchFriendInfo"
               placeholder="用户id/用户名"
               @keyup.enter="searchFriendInfoResult"
+              @focus.stop="clearMouseEvent"
+              ref="serachInput"
             />
             <img
               src="@/assets/img/list/search_tokenfield_delete@2x.png"
@@ -104,9 +113,11 @@ import { responseDataType } from "@/type/ComponentDataType";
 export default defineComponent({
   name: "addFriends-list",
   methods: {
+    // 关闭添加好友弹框
     removeAddAlert() {
       this.$emit("no-show-add-friends-alert", !this.show);
     },
+    // 获取搜索好友列表
     searchFriendInfoResult() {
       this.friendsList = [];
       if (this.searchFriendInfo) {
@@ -117,12 +128,11 @@ export default defineComponent({
             userId: this.$store.state.userID
           })
           .then((res: responseDataType) => {
-            console.log(res.data);
-
-            this.$refs.addFriendsListContent.style = "margin-top:-220px;";
             this.$refs.mainPanel.style = "height:auto;";
             this.$refs.mainPanelContentFriendsInfo.style =
               " height: 269px;overflow: auto; ";
+            this.$refs.addFriendsListContent.style.marginTop =
+              -(this.$refs.addFriendsListContent.clientHeight / 2) + "px";
             if (res.data.length > 0) {
               res.data.forEach((item: any) => {
                 if (!item.isFriend) {
@@ -146,20 +156,66 @@ export default defineComponent({
             }
           });
       } else {
-        this.$refs.addFriendsListContent.style = "";
+        this.$refs.addFriendsListContent.style.marginTop = "";
         this.$refs.mainPanel.style = "";
         this.showUserInfo = false;
       }
     },
+    // 清空搜索框的内容
     clearSearchFriendInfo() {
       this.searchFriendInfo = "";
+      this.$refs.serachInput.focus();
     },
+    // 判断关闭与最小化图标是否出现
     showLeftIco: function(status: boolean) {
       if (status) {
         this.leftIco = "block";
       } else {
         this.leftIco = "none";
       }
+    },
+    // 按下触发可拖曳事件
+    alertDown: function(e: any) {
+      this.moveAlertData.x = e.clientX;
+      this.moveAlertData.y = e.clientY;
+      if (this.friendsList.length != 0) {
+        this.moveAlertData.t =
+          this.$refs.addFriendsListContent.offsetTop +
+          this.$refs.addFriendsListContent.clientHeight / 2;
+      } else {
+        this.moveAlertData.t = this.$refs.addFriendsListContent.offsetTop + 126;
+      }
+      this.moveAlertData.l = this.$refs.addFriendsListContent.offsetLeft + 350;
+      this.moveAlertData.isDown = true;
+      this.$refs.addFriendsListContent.style.cursor = "move";
+    },
+    // 按下并持续触发可拖曳事件
+    alertMove: function(e: any) {
+      if (this.moveAlertData.isDown == false) {
+        return;
+      }
+      this.moveAlertData.moveX = e.clientX;
+      this.moveAlertData.moveY = e.clientY;
+      this.moveAlertData.movel =
+        this.moveAlertData.moveX -
+        (this.moveAlertData.x - this.moveAlertData.l);
+      this.moveAlertData.movet =
+        this.moveAlertData.moveY -
+        (this.moveAlertData.y - this.moveAlertData.t);
+
+      this.$refs.addFriendsListContent.style.left =
+        this.moveAlertData.movel + "px";
+      this.$refs.addFriendsListContent.style.top =
+        this.moveAlertData.movet + "px";
+    },
+    // 可拖曳事件完成事件
+    alertUp: function() {
+      this.moveAlertData.isDown = false;
+      this.$refs.addFriendsListContent.style.cursor = "default";
+    },
+    // 获取搜索框焦点事件
+    clearMouseEvent: function() {
+      this.moveAlertData.isDown = false;
     }
   },
   props: {
@@ -170,7 +226,18 @@ export default defineComponent({
       searchFriendInfo: "",
       friendsList: [],
       showUserInfo: false,
-      leftIco: "none"
+      leftIco: "none",
+      moveAlertData: {
+        x: 0,
+        y: 0,
+        l: 0,
+        t: 0,
+        moveX: 0,
+        moveY: 0,
+        movel: 0,
+        movet: 0,
+        isDown: false
+      }
     };
   },
   emits: {
