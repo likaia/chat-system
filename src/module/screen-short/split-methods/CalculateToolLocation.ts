4c552ea93d9b1d39c2f3d33ca557dbb93bd12b1d
likaia <1195419506@qq.com>
2021-01-31 00:56:44 +0800

    fix: 修复从右下角拖拽裁剪框时，截图工具栏位置计算失误问题

--- src/module/screen-short/split-methods/CalculateToolLocation.ts
+++ src/module/screen-short/split-methods/CalculateToolLocation.ts
@@ -12,7 +12,11 @@ export function calculateToolLocation(
   // 工具栏X轴坐标 = (裁剪框的宽度 - 工具栏的宽度) / 2 + 裁剪框距离左侧的距离
   const mouseX = (position.width - toolWidth) / 2 + position.startX;
   // 工具栏Y轴坐标
-  const mouseY = position.startY + position.height + 10;
+  let mouseY = position.startY + position.height + 10;
+  if (position.width < 0 && position.height < 0) {
+    // 从右下角拖动时，工具条y轴的位置应该为position.startY + 10
+    mouseY = position.startY + 10;
+  }
   return {
     mouseX,
     mouseY
