16f61c48a50a7a4c6a5afe35702c933a535280be
wenhim <6531221+wenhim@user.noreply.gitee.com>
2021-02-06 10:39:30 +0800

    feat(更多列表): 文件区域
    1.新增文件区域列表显示 2.修复设置自动弹回

--- src/api/fileManageAPI.ts
+++ src/api/fileManageAPI.ts
@@ -4,12 +4,27 @@
 import services from "../config/axios";
 import base from "./base"; // 导入接口域名列表
 
+// 定义参数类型
+type paramsType<T> = {
+  [key: string]: T;
+};
+
 const fileManageAPI = {
   // 文件上传接口
   upload(file: File | FormData) {
     return services.post(`${base.lkBaseURL}/uploads/singleFileUpload`, file, {
       headers: { "Content-Type": "multipart/form-data" }
     });
+  },
+  // 文件列表
+  getUserFileList<T>(params: paramsType<T>) {
+    return services.get(`${base.lk}/fileManagement/getUserFileList`, {
+      params: params
+    });
+  },
+  // 添加文件
+  getAddFileInfo<T>(params: paramsType<T>) {
+    return services.post(`${base.lk}/fileManagement/addFileInfo`, params);
   }
 };
 
