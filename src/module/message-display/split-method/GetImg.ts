2ee8fae50e7895e5b00e165fdcd343ad54cde745
likaia <1195419506@qq.com>
2021-02-20 20:30:04 +0800

    fix: 实现截图功能的点击对号图标，粘贴图片至聊天输入框功能

--- src/module/message-display/split-method/GetImg.ts
+++ src/module/message-display/split-method/GetImg.ts
@@ -1,6 +1,12 @@
 /**
  * 获取裁剪后的图片
  */
+import getEditableDivFocus from "@/module/message-display/components-metords/GetEditableDivFocus";
+
 export default function getImg(base64: string) {
-  console.log("截图组件传递的图片信息", base64);
+  // 可编辑div获取焦点
+  getEditableDivFocus();
+  const imgTag = `<img src="${base64}">`;
+  // 将图片插入剪切板
+  document.execCommand("insertHTML", false, imgTag);
 }
