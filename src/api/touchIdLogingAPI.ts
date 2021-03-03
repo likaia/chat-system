ee942aeb3840e395be86751c42a3286032eb58c1
likaia <1195419506@qq.com>
2021-03-03 11:08:37 +0800

    fix: 修改接口地址为开发环境

--- src/api/touchIdLogingAPI.ts
+++ src/api/touchIdLogingAPI.ts
@@ -4,17 +4,17 @@ import base from "@/api/base";
 const touchIdLogingAPI = {
   // 获取touchId
   getTouchID(params: { userId: string }) {
-    return services.post(`${base.lk}/user/getTouchID`, params);
+    return services.post(`${base.develop}/user/getTouchID`, params);
   },
 
   // 注册touchid
   registeredTouchID(params: { touchId: string; clientDataJson: string }) {
-    return services.post(`${base.lk}/user/registeredTouchID`, params);
+    return services.post(`${base.develop}/user/registeredTouchID`, params);
   },
 
   // 指纹登录
   touchIdLogin(params: { touchId: string; certificate: string }) {
-    return services.post(`${base.lk}/user/touchIdLogin`, params);
+    return services.post(`${base.develop}/user/touchIdLogin`, params);
   }
 };
 
