bdeaaecba0b1041b18982b53eb3efa311cb69d2f
likai <1195419506@qq.com>
2022-08-04 22:15:29 +0800

    feat: 使用vite作为项目架构

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import vue from "@vitejs/plugin-vue";
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    host: true,
+    port: 8020,
+    proxy: {}
+  },
+  resolve: {
+    // 设置路径别名
+    alias: {
+      "@": resolve(__dirname, "./src"),
+      "*": resolve("")
+    }
+  },
+  base: IS_PRODUCTION ? "/chat-system" : "./",
+  define: {
+    "process.env": {}
+  },
+  build: {
+    outDir: resolve(__dirname, "dist")
+  }
+});
